4247d630026a5375d034ea24d4a0168ee1878559
JiveDig <jivetheuniversal@gmail.com>
2025-07-02 12:37:14 -0400

    0.3.0-beta.3

--- mai-performance-images.php
+++ mai-performance-images.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Mai Performance Images
  * Description:       Optimizes image delivery through automatic resizing and WebP conversion with static file caching.
- * Version:           0.3.0-beta.2
+ * Version:           0.3.0-beta.3
  * Requires at least: 6.7
  * Requires PHP:      8.0
  * Author:            JiveDig
