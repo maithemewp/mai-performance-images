60ea7338cacb617293dd52c88111983bcd593b25
JiveDig <jivetheuniversal@gmail.com>
2025-09-16 14:20:18 -0400

    Add settings page

---
+++ classes/class-settings.php
@@ -0,0 +1,252 @@
+<?php
+
+namespace Mai\PerformanceImages;
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * Adds settings page.
+ *
+ * @since 0.5.0
+ */
+class Settings {
+	/**
+	 * The default options.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @var array
+	 */
+	protected $defaults;
+
+	/**
+	 * The options.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @var array
+	 */
+	protected $options;
+
+	/**
+	 * Construct the class.
+	 */
+	function __construct() {
+		$this->hooks();
+	}
+
+	/**
+	 * Add hooks.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @return void
+	 */
+	function hooks() {
+		add_action( 'admin_menu', [ $this, 'add_menu_item' ], 12 );
+		add_action( 'admin_init', [ $this, 'init' ] );
+		add_filter( 'plugin_action_links_mai-performance-images/mai-performance-images.php', [ $this, 'add_plugin_links' ], 10, 4 );
+	}
+
+	/**
+	 * Adds menu item for settings page.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @return void
+	 */
+	function add_menu_item() {
+		add_options_page(
+			__( 'Mai Performance Images', 'mai-performance-images' ), // page_title
+			__( 'Mai Perf Images', 'mai-performance-images' ), // menu_title
+			'manage_options', // capability
+			'mai-performance-images', // menu_slug
+			[ $this, 'add_content' ], // callback
+		);
+	}
+
+	/**
+	 * Adds setting page content.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @return void
+	 */
+	function add_content() {
+		echo '<div class="wrap">';
+			printf( '<h2>%s</h2>', __( 'Mai Performance Images', 'mai-performance-images' ) );
+			printf( '<p>%s</p>', __( 'Configure image optimization settings for better performance.', 'mai-performance-images' ) );
+			echo '<form method="post" action="options.php">';
+				settings_fields( 'mai_performance_images_group' );
+				do_settings_sections( 'mai-performance-images-section' );
+				submit_button();
+			echo '</form>';
+		echo '</div>';
+	}
+
+	/**
+	 * Initialize the settings.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @return void
+	 */
+	function init() {
+		// Set defaults/options.
+		$this->defaults = get_default_options();
+		$this->options  = get_option( 'mai_performance_images', $this->defaults );
+
+		// Register setting.
+		register_setting(
+			'mai_performance_images_group', // option_group
+			'mai_performance_images', // option_name
+			[ $this, 'sanitize' ] // sanitize_callback
+		);
+
+		/************
+		 * Sections *
+		 ************/
+
+		// Register section.
+		add_settings_section(
+			'mai_performance_images_general', // id
+			'', // title
+			[ $this, 'general_section_callback' ], // callback
+			'mai-performance-images-section' // page
+		);
+
+		/************
+		 * Fields   *
+		 ************/
+
+		// Attributes.
+		add_settings_field(
+			'attributes', // id
+			__( 'Attributes', 'mai-performance-images' ), // title
+			[ $this, 'attributes_callback' ], // callback
+			'mai-performance-images-section', // page
+			'mai_performance_images_general' // section
+		);
+
+		// Conversion.
+		add_settings_field(
+			'conversion', // id
+			__( 'Conversion', 'mai-performance-images' ), // title
+			[ $this, 'conversion_callback' ], // callback
+			'mai-performance-images-section', // page
+			'mai_performance_images_general' // section
+		);
+
+		// Quality.
+		add_settings_field(
+			'quality', // id
+			__( 'Image Quality', 'mai-performance-images' ), // title
+			[ $this, 'quality_callback' ], // callback
+			'mai-performance-images-section', // page
+			'mai_performance_images_quality' // section
+		);
+	}
+
+	/**
+	 * General section callback.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @return void
+	 */
+	function general_section_callback() {}
+
+	/**
+	 * Attributes field callback.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @return void
+	 */
+	function attributes_callback() {
+		$attributes = $this->options['attributes'] ?? $this->defaults['attributes'] ?? true;
+		?>
+		<label>
+			<input type="checkbox" name="mai_performance_images[attributes]" value="1" <?php checked( $attributes, 1 ); ?> />
+			<?php _e( 'Enable lazy/eager/priority attributes for images', 'mai-performance-images' ); ?>
+		</label>
+		<p class="description"><?php _e( 'Adds block and Customizer settings for image loading.', 'mai-performance-images' ); ?></p>
+		<?php
+	}
+
+	/**
+	 * Conversion field callback.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @return void
+	 */
+	function conversion_callback() {
+		$conversion = $this->options['conversion'] ?? $this->defaults['conversion'] ?? true;
+		?>
+		<label>
+			<input type="checkbox" name="mai_performance_images[conversion]" value="1" <?php checked( $conversion, 1 ); ?> />
+			<?php _e( 'Enable image conversion to WebP', 'mai-performance-images' ); ?>
+		</label>
+		<p class="description"><?php _e( 'Images will be converted to appropriately sized WebP images.', 'mai-performance-images' ); ?></p>
+		<?php
+	}
+
+	/**
+	 * WebP quality field callback.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @return void
+	 */
+	function quality_callback() {
+		$quality = $this->options['quality'] ?? $this->defaults['quality'] ?? 85;
+		?>
+		<input type="number" name="mai_performance_images[quality]" value="<?php echo esc_attr( $quality ); ?>" min="1" max="100" />
+		<p class="description"><?php _e( 'WebP image quality (1-100). Higher values mean better quality but larger file sizes.', 'mai-performance-images' ); ?></p>
+		<?php
+	}
+
+	/**
+	 * Sanitize the settings.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @param array $input The input array.
+	 *
+	 * @return array The sanitized array.
+	 */
+	function sanitize( $input ) {
+		$sanitized = [];
+
+		// Parse existing options and defaults.
+		$input = wp_parse_args( $input, $this->options );
+		$input = wp_parse_args( $input, $this->defaults );
+
+		// Boolean fields.
+		$sanitized['conversion'] = (bool) $input['conversion'];
+		$sanitized['attributes'] = (bool) $input['attributes'];
+		$sanitized['quality']    = max( 1, min( 100, (int) $input['quality'] ) );
+
+		return $sanitized;
+	}
+
+	/**
+	 * Add plugin action links.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @param array  $actions     An array of plugin action links.
+	 * @param string $plugin_file Path to the plugin file relative to the plugins directory.
+	 * @param array  $plugin_data An array of plugin data from the plugin file headers.
+	 * @param string $context     Plugin status context, ie 'all', 'active', 'inactive', 'recently_active'.
+	 *
+	 * @return array associative array of plugin action links.
+	 */
+	function add_plugin_links( $actions, $plugin_file, $plugin_data, $context ) {
+		$actions['settings'] = sprintf( '<a href="%s">%s</a>', esc_url( admin_url( 'options-general.php?page=mai-performance-images' ) ), __( 'Settings', 'mai-performance-images' ) );
+
+		return $actions;
+	}
+}
