04c4b39dfd2955e4d5abc86a26ff202a8e399b2e
JiveDig <jivetheuniversal@gmail.com>
2025-09-16 15:55:47 -0400

    respect settings in MaiBlocks

--- classes/class-mai-blocks.php
+++ classes/class-mai-blocks.php
@@ -13,6 +13,23 @@ if ( ! defined( 'ABSPATH' ) ) exit;
  * @since 0.4.0
  */
 class MaiBlocks extends Images {
+	/**
+	 * The attributes enabled.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @var bool
+	 */
+	protected $attributes_enabled;
+
+	/**
+	 * The conversion enabled.
+	 *
+	 * @since 0.5.0
+	 *
+	 * @var bool
+	 */
+	protected $conversion_enabled;
 
 	/**
 	 * Add hooks.
@@ -22,6 +39,16 @@ class MaiBlocks extends Images {
 	 * @return void
 	 */
 	protected function hooks(): void {
+		// Set props.
+		$this->attributes_enabled = is_attributes_enabled();
+		$this->conversion_enabled = is_conversion_enabled();
+
+		// Bail if nothing is enabled.
+		if ( ! $this->attributes_enabled && ! $this->conversion_enabled ) {
+			return;
+		}
+
+		// Bail if nothing is enabled.
 		add_filter( 'render_block_acf/mai-post-preview', [ $this, 'render_block_post_preview' ], 99, 2 );
 	}
 
@@ -36,34 +63,42 @@ class MaiBlocks extends Images {
 	 * @return string The block content.
 	 */
 	public function render_block_post_preview( string $block_content, array $block ): string {
-		/**
-		 * Set up tag processor.
-		 * @disregard P1008
-		 */
-		$tags = new WP_HTML_Tag_Processor( $block_content );
+		// If attributes are enabled.
+		if ( $this->attributes_enabled ) {
+			/**
+			 * Set up tag processor.
+			 * @disregard P1008
+			 */
+			$tags = new WP_HTML_Tag_Processor( $block_content );
 
-		// Loop through tags.
-		while ( $tags->next_tag( [ 'tag_name' => 'img' ] ) ) {
-			$tags->set_attribute( 'data-mai-loading', 'lazy' );
+			// Loop through tags.
+			while ( $tags->next_tag( [ 'tag_name' => 'img' ] ) ) {
+				$tags->set_attribute( 'data-mai-loading', 'lazy' );
+			}
+
+			// Get updated content.
+			$block_content = $tags->get_updated_html();
+
+			// Handle attributes.
+			$block_content = $this->handle_attributes( $block_content );
 		}
 
-		// Get updated content.
-		$block_content = $tags->get_updated_html();
+		// If conversion is enabled.
+		if ( $this->conversion_enabled ) {
+			// Set args.
+			$image_args = [
+				'aspect_ratio' => '3/4',
+				'max_width'    => 300,
+				'sizes'        => [
+					'mobile'  => '100vw',
+					'tablet'  => '300px',
+					'desktop' => '300px',
+				],
+			];
 
-		// Set args.
-		$image_args = [
-			'aspect_ratio' => '3/4',
-			'max_width'    => 300,
-			'sizes'        => [
-				'mobile'  => '100vw',
-				'tablet'  => '300px',
-				'desktop' => '300px',
-			],
-		];
-
-		/** @disregard P1008 */
-		$block_content = $this->handle_image( $block_content, $image_args );
-		$block_content = $this->handle_attributes( $block_content );
+			/** @disregard P1008 */
+			$block_content = $this->handle_image( $block_content, $image_args );
+		}
 
 		// Return the block content.
 		return $block_content;
