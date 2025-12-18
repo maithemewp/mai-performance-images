b8f627d204224e49cf5ba66aef6af1675053f26c
JiveDig <jivetheuniversal@gmail.com>
2025-12-18 10:10:27 -0500

    remove dev dependencies

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -17,6 +17,5 @@ if (PHP_VERSION_ID < 50600) {
 }
 
 require_once __DIR__ . '/../vendor-prefixed//autoload.php';
-require_once __DIR__ . '/composer/autoload_aliases.php';
 require_once __DIR__ . '/composer/autoload_real.php';
 return ComposerAutoloaderInit5ecbabff5f374cf0a6ff2146b68b0b1b::getLoader();
